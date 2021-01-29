42d3a9b1222c76da45989370c9113008e34f9f9e
unknown <alej.mernu@gmail.com>
2021-01-28 19:32:38 -0600

    tailwind, react-redux, redux, input and label

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Input from "./components/Input";
+import Label from "./components/Label";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <main>
+        <Switch>
+          <Route path="/" exact>
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
